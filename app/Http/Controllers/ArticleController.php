e23cef313dcecbfa58f7375544cf9c123c6e2596
King-Success <okonkwoak@gmail.com>
2018-04-18 22:21:04 +0100

    Created the Articles controller and added the actions to its resources

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Article;
+
+class ArticleController extends Controller
+{
+
+    public function index()
+    {
+        return Article::all();
+    }
+
+
+    public function store(Request $request)
+    {
+        return Article::create($request->all());
+    }
+
+
+    public function show($id)
+    {
+        return Article::find($id);
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $article = Article::findOrFail($id);
+
+        return $article;
+    }
+
+    public function destroy($id)
+    {
+        $article = Article::findOrFail($id);
+        $article->delete();
+        return 204;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,27 @@ use Illuminate\Http\Request;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('articles', function(){
+    return Article::all();
+});
+
+Route::get('articles/{$id}', function($id){
+    return Article::find($id);
+});
+
+Route::post('articles', function(Request $request){
+    return Article::Create($request->all());
+});
+
+Route::put('articles/{$id}', function(Request $request, $id){
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+    return $article;
+});
+
+Route::delete('articles/{$id}', function($id){
+    Article::find($id)->delete();
+    return 204;
 });
